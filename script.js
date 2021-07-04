9998b083cd384fce35c69d906de7452a9caca53c
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-07-04 11:57:17 -0400

    search

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const key = "e788aa352fb3d4ecf2ce090225ef5c36"
+const key = "e788aa352fb3d4ecf2ce090225ef5c36";
+
+var city = [];
+var searchtext;
+
+
+
+
+$("#searchBtn").on("click", function (){
+    searchtext = $("#search").val();
+    console.log(searchtext)
+});
+
+console.log(searchtext)
