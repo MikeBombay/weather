fcb46bb6f93e7e688e1847c783fb4d137eba2407
Mike Bombay <82775499+MikeBombay@users.noreply.github.com>
2021-07-04 12:47:32 -0400

    list working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const key = "e788aa352fb3d4ecf2ce090225ef5c36";
 
-var city = [];
+var cities = [];
 var searchtext;
 
 
@@ -9,6 +9,34 @@ var searchtext;
 $("#searchBtn").on("click", function (){
     searchtext = $("#search").val();
     console.log(searchtext)
+    if (cities.includes(searchtext)){
+
+        console.log("inArray")
+    }
+    else {
+        cities.unshift(searchtext);
+        localStorage.setItem("citiesKey", JSON.stringify(cities));
+    }
+    fillCityList();
+
 });
 
-console.log(searchtext)
+
+
+
+function fillCityList() {
+    cities = JSON.parse(localStorage.getItem("citiesKey"));
+    if (cities == null) cities = [];
+    //limit length
+    if (cities.length > 10) {
+        cities.pop();
+    };
+    $("#cityList").html("");
+    for (let i=0; i < cities.length; i++){
+        $("#cityList").append(`<li id="${i}"class="list-group-item list-group-item-secondary text-center my-2 font-weight-bold mt-3">${cities[i]}</li>`)
+    }};
+
+    
+
+
+//`<li id="${i}"class="list-group-item list-group-item-secondary text-center my-2 font-weight-bold mt-3">
